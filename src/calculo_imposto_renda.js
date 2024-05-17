1d860060e2efa803a06a854c8223385404263af7
viniciussilvax <viniciusdasilvax@hotmail.com>
2024-05-17 19:52:40 -0300

    feat: 🎸 M2S01 Ex3 Resolvido

---
+++ src/calculo_imposto_renda.js
@@ -0,0 +1,17 @@
+function calcIrrf(valorBruto) {
+    
+    if (valorBruto <= 2112) {
+        return 'Isento do IRRF'
+    } else if (valorBruto > 2112 && valorBruto <= 2826.55) {
+        return 'R$ 158.40 de Imposto de renda'
+    } else if (valorBruto > 2826.55 && valorBruto <= 3751.05) {
+        return 'R$ 370,40 de Imposto de renda'
+    } else if (valorBruto > 3751.05 && valorBruto <= 4664.68) {
+        return 'R$ 651,73 de Imposto de renda'
+    } else {
+        return 'R$ 884,96 de Imposto de renda'
+    }
+
+}
+
+console.log(calcIrrf(1500))
