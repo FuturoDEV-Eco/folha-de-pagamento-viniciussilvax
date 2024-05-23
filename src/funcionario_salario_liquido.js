6d056866f84647da11692873645f8f7150aff19f
viniciussilvax <viniciusdasilvax@hotmail.com>
2024-05-23 19:44:48 -0300

    feat: 🎸 Exedr 5

--- src/funcionario_salario_liquido.js
+++ src/funcionario_salario_liquido.js
@@ -7,27 +7,48 @@ const input = readline.createInterface(
     process.stdout
 );
 
+const pdcDoc = require("pdfkit")
+
 const calcLiquido = require("./calculo_salario_liquido")
 const calcInss = require("./calculo_inss")
 const calcIrrf = require("./calculo_imposto_renda")
 const formatCpf = require("./utils/format_cpf")
 
-input.question("Nome Funcionário: ", (name) => {
+input.question("Nome Funcionário: ", (nome) => {
     input.question("CPF: ", (cpf) => {
         input.question("Mês Pagamento: ", (mesPagamento) => {
             input.question("Salário Bruto: ", (salarioBruto) => {
-                console.log(`--  FOLHA PAGAMENTO  ${mesPagamento}---`)
-                console.log(`--- Nome: ${name} ---`)
-                console.log(`--- CPF: ${formatCpf(cpf)} ---`)
-                console.log(`--- INSS: ${calcInss(salarioBruto)} ---`)
-                console.log(`--- Imposto de Renda: ${calcIrrf(salarioBruto)} ---`)
-                console.log(`--- Salário Liquido ${calcLiquido(salarioBruto)} ---`)
-    
-                input.close()
+                // console.log(`--  FOLHA PAGAMENTO  ${mesPagamento}---`)
+                // console.log(`--- Nome: ${name} ---`)
+                // console.log(`--- CPF: ${formatCpf(cpf)} ---`)
+                // console.log(`--- INSS: ${calcInss(salarioBruto)} ---`)
+                // console.log(`--- Imposto de Renda: ${calcIrrf(salarioBruto)} ---`)
+                // console.log(`--- Salário Liquido ${calcLiquido(salarioBruto)} ---`)
+
+                input.question("Gerar PDF : (s/n) ", (gerar) => {
+                    if (gerar.toLocaleLowerCase() === 's') {
+                        const doc = new PDFDocument();
+                        doc.pipe(fs.createWriteStream(`Folha de pagamento ${nome}.pdf`));
+                        doc.fontSize(16);
+                        doc.text(`---Folha de pagamento ${mesPagamento}---`);
+                        doc.text(`Data de Geração: ${new Date().toLocaleDateString()}`);
+                        doc.text(`Funcionario: ${nome}`);
+                        doc.text(`CPF: ${formatCpf(cpf)}`);
+                        doc.text(`------`);
+                        doc.text(`Salario Bruto: R$ ${salarioBruto}`);
+                        doc.text(`INSS: R$ ${calcInss(salarioBruto)}`);
+                        doc.text(`Imposto de renda: R$ ${calcIrrf(salarioBruto)}`);
+                        doc.text(`Salario Liquido: R$ ${calcLiquido(salarioBruto)}`);
+                        doc.end();
+                    }
+                })
             })
         })
     })
 })
+
+
+
 
 
 
